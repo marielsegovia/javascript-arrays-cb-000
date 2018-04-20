a67f368aabca6158aaa0a4cbde58047d38f147ee
Mariel Segovia <msegovia94@gmail.com>
2018-04-20 05:50:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,8 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  
+  newArray = [...array, element]
+  return newArray
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
