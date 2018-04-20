f7b330f0092b93516c596b5be986fef9ecdcce9e
Mariel Segovia <msegovia94@gmail.com>
2018-04-20 06:27:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  // array as only argument and removes last element --- returns entire array and SHOULD mutate array 
+  // array as only argument and removes last element --- returns entire array and SHOULD mutate array
   array.pop()
   return array
 }
